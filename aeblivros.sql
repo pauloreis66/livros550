06992fc86a3075f062b1932d1c690a53b43f03f5
Paulo Reis <pmpreis@gmail.com>
2018-05-10 17:51:48 +0100

    Add files via upload

--- aeblivros.sql
+++ aeblivros.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 27-Mar-2018 às 13:43
--- Versão do servidor: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: 10-Maio-2018 às 18:48
+-- Versão do servidor: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `aeblivros`
@@ -26,10 +28,10 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `categorias`
 --
 
-CREATE TABLE IF NOT EXISTS `categorias` (
-`idCat` int(4) NOT NULL,
+CREATE TABLE `categorias` (
+  `idCat` tinyint(4) NOT NULL,
   `categoria` varchar(60) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `categorias`
@@ -53,13 +55,23 @@ INSERT INTO `categorias` (`idCat`, `categoria`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `categorias1`
+-- Estrutura da tabela `detalhesrequisicao`
 --
 
-CREATE TABLE IF NOT EXISTS `categorias1` (
-`idCat` int(4) NOT NULL,
-  `categoria` varchar(60) NOT NULL
+CREATE TABLE `detalhesrequisicao` (
+  `idReq` int(10) NOT NULL,
+  `idLivro` int(6) NOT NULL,
+  `quantidade` tinyint(4) NOT NULL,
+  `dataDevolucao` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `detalhesrequisicao`
+--
+
+INSERT INTO `detalhesrequisicao` (`idReq`, `idLivro`, `quantidade`, `dataDevolucao`) VALUES
+(1, 3, 1, '0000-00-00'),
+(1, 2, 9, '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -67,11 +79,11 @@ CREATE TABLE IF NOT EXISTS `categorias1` (
 -- Estrutura da tabela `editoras`
 --
 
-CREATE TABLE IF NOT EXISTS `editoras` (
-`idEditora` int(4) NOT NULL,
+CREATE TABLE `editoras` (
+  `idEditora` tinyint(4) NOT NULL,
   `editora` varchar(40) NOT NULL,
   `url` varchar(60) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `editoras`
@@ -97,16 +109,16 @@ INSERT INTO `editoras` (`idEditora`, `editora`, `url`) VALUES
 -- Estrutura da tabela `idiomas`
 --
 
-CREATE TABLE IF NOT EXISTS `idiomas` (
-`idIdioma` int(4) NOT NULL,
-  `Idioma` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+CREATE TABLE `idiomas` (
+  `idIdioma` tinyint(2) NOT NULL,
+  `idioma` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `idiomas`
 --
 
-INSERT INTO `idiomas` (`idIdioma`, `Idioma`) VALUES
+INSERT INTO `idiomas` (`idIdioma`, `idioma`) VALUES
 (1, 'Português'),
 (2, 'Português do Brasil'),
 (3, 'Inglês'),
@@ -117,41 +129,40 @@ INSERT INTO `idiomas` (`idIdioma`, `Idioma`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `livros1`
+-- Estrutura da tabela `livros`
 --
 
-CREATE TABLE IF NOT EXISTS `livros1` (
-`idLivro` int(6) NOT NULL,
-  `idCat` int(11) NOT NULL,
-  `idEditora` int(11) NOT NULL,
+CREATE TABLE `livros` (
+  `idLivro` int(6) NOT NULL,
+  `idCat` tinyint(4) NOT NULL,
+  `idEditora` tinyint(4) NOT NULL,
   `titulo` varchar(100) NOT NULL,
   `autor` varchar(60) NOT NULL,
   `isbn` int(13) DEFAULT NULL,
   `anoEdicao` int(4) DEFAULT NULL,
-  `idIdioma` int(11) NOT NULL,
-  `idOrigem` int(11) NOT NULL,
+  `idIdioma` tinyint(2) NOT NULL,
+  `idOrigem` tinyint(2) NOT NULL,
   `sinopse` text,
-  `capa` mediumblob,
   `imgCapa` varchar(255) DEFAULT NULL,
-  `exemplares` int(4) DEFAULT '1',
+  `exemplares` tinyint(4) UNSIGNED DEFAULT '1',
   `promo` bit(1) NOT NULL DEFAULT b'0',
-  `DataReg` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+  `dataReg` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Extraindo dados da tabela `livros1`
+-- Extraindo dados da tabela `livros`
 --
 
-INSERT INTO `livros1` (`idLivro`, `idCat`, `idEditora`, `titulo`, `autor`, `isbn`, `anoEdicao`, `idIdioma`, `idOrigem`, `sinopse`, `capa`, `imgCapa`, `exemplares`, `promo`, `DataReg`) VALUES
-(1, 13, 1, 'Ajax - Guia Prático para Windows', 'Wallace Soares', 2147483647, 2010, 2, 2, NULL, NULL, '0001 - Ajax - Guia PrÃ¡tico para Windows.jpg', 1, b'0', '2018-03-06 19:50:44'),
-(2, 13, 1, 'JavaScript - Guia Prático do Webmaster', 'Osmar J. Silva', 2147483647, 2000, 2, 3, NULL, NULL, '0002 - JavaScript - Guia PrÃ¡tico do Webmaster.jpg', 1, b'0', '2018-03-06 19:50:44'),
-(3, 13, 2, 'FrontPage 2003 - Curso Completo', 'Ana Mendes, Maria Mendes, Francisco Marques', 2147483647, 2006, 1, 3, NULL, NULL, '0003 - FrontPage 2003 - Curso Completo.jpg', 1, b'0', '2018-03-06 19:50:44'),
-(4, 13, 2, 'Web Design - estrutura, concepção e produção de sites Web', 'Bruno Figueiredo', 2147483647, 2002, 1, 4, NULL, NULL, '0004 - Web Design - estrutura, concepÃ§Ã£o e produÃ§Ã£o de sites Web.jpg', 1, b'0', '2018-03-06 19:50:44'),
-(5, 13, 2, 'Como criar Páginas Web, Depressa e Bem', 'Pedro Coelho', 2147483647, 2003, 1, 4, NULL, NULL, '0005 - Como criar PÃ¡ginas Web, Depressa e Bem.jpg', 1, b'0', '2018-03-06 19:50:44'),
-(6, 13, 1, 'Web Total - Desenvolva Sites com Tecnologias de Uso Livre', 'Evandro Carlos Teruel', 2147483647, 2009, 2, 4, NULL, NULL, '0006 - Web Total - Desenvolva Sites com Tecnologias de Uso Livre.jpg', 1, b'0', '2018-03-06 19:50:44'),
-(7, 13, 1, 'Guia de Orientação e Desenvolvimento de Sites', 'José Augusto Manzano, Suely Alves de Toledo', 2147483647, 2008, 2, 4, NULL, NULL, '0007 - Guia de OrientaÃ§Ã£o e Desenvolvimento de Sites.jpg', 1, b'0', '2018-03-06 19:50:44'),
-(8, 13, 2, 'Dreamweaver CS3 - Depressa e Bem', 'Helder Oliveira', 2147483647, 2008, 1, 4, NULL, NULL, '0008 - Dreamweaver CS3 - Depressa e Bem.jpg', 1, b'0', '2018-03-06 19:55:39'),
-(9, 13, 3, 'O Guia Prático do Dreamweaver CS3 com PHP, JavaScript e Ajax', 'Pedro Remoaldo', 2147483647, 2008, 1, 4, NULL, NULL, '0009 - O Guia PrÃ¡tico do Dreamweaver CS3 com PHP, JavaScript e Ajax.jpg', 1, b'0', '2018-03-08 14:32:14');
+INSERT INTO `livros` (`idLivro`, `idCat`, `idEditora`, `titulo`, `autor`, `isbn`, `anoEdicao`, `idIdioma`, `idOrigem`, `sinopse`, `imgCapa`, `exemplares`, `promo`, `dataReg`) VALUES
+(1, 13, 1, 'Ajax - Guia Prático para Windows', 'Wallace Soares', 2147483647, 2010, 2, 2, NULL, '0001 - Ajax - Guia PrÃ¡tico para Windows.jpg', 1, b'0', '2018-03-06 19:50:44'),
+(2, 13, 1, 'JavaScript - Guia Prático do Webmaster', 'Osmar J. Silva', 2147483647, 2000, 2, 3, NULL, '0002 - JavaScript - Guia PrÃ¡tico do Webmaster.jpg', 1, b'0', '2018-03-06 19:50:44'),
+(3, 13, 2, 'FrontPage 2003 - Curso Completo', 'Ana Mendes, Maria Mendes, Francisco Marques', 2147483647, 2006, 1, 3, NULL, '0003 - FrontPage 2003 - Curso Completo.jpg', 1, b'0', '2018-03-06 19:50:44'),
+(4, 13, 2, 'Web Design - estrutura, concepção e produção de sites Web', 'Bruno Figueiredo', 2147483647, 2002, 1, 4, NULL, '0004 - Web Design - estrutura, concepÃ§Ã£o e produÃ§Ã£o de sites Web.jpg', 1, b'0', '2018-03-06 19:50:44'),
+(5, 13, 2, 'Como criar Páginas Web, Depressa e Bem', 'Pedro Coelho', 2147483647, 2003, 1, 4, NULL, '0005 - Como criar PÃ¡ginas Web, Depressa e Bem.jpg', 1, b'0', '2018-03-06 19:50:44'),
+(6, 13, 1, 'Web Total - Desenvolva Sites com Tecnologias de Uso Livre', 'Evandro Carlos Teruel', 2147483647, 2009, 2, 4, NULL, '0006 - Web Total - Desenvolva Sites com Tecnologias de Uso Livre.jpg', 1, b'0', '2018-03-06 19:50:44'),
+(7, 13, 1, 'Guia de Orientação e Desenvolvimento de Sites', 'José Augusto Manzano, Suely Alves de Toledo', 2147483647, 2008, 2, 4, NULL, '0007 - Guia de OrientaÃ§Ã£o e Desenvolvimento de Sites.jpg', 1, b'0', '2018-03-06 19:50:44'),
+(8, 13, 2, 'Dreamweaver CS3 - Depressa e Bem', 'Helder Oliveira', 2147483647, 2008, 1, 4, NULL, '0008 - Dreamweaver CS3 - Depressa e Bem.jpg', 1, b'0', '2018-03-06 19:55:39'),
+(9, 13, 3, 'O Guia Prático do Dreamweaver CS3 com PHP, JavaScript e Ajax', 'Pedro Remoaldo', 2147483647, 2008, 1, 4, NULL, '0009 - O Guia PrÃ¡tico do Dreamweaver CS3 com PHP, JavaScript e Ajax.jpg', 1, b'0', '2018-03-08 14:32:14');
 
 -- --------------------------------------------------------
 
@@ -159,10 +170,10 @@ INSERT INTO `livros1` (`idLivro`, `idCat`, `idEditora`, `titulo`, `autor`, `isbn
 -- Estrutura da tabela `origem`
 --
 
-CREATE TABLE IF NOT EXISTS `origem` (
-`idOrigem` int(4) NOT NULL,
+CREATE TABLE `origem` (
+  `idOrigem` tinyint(2) NOT NULL,
   `origem` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `origem`
@@ -178,28 +189,69 @@ INSERT INTO `origem` (`idOrigem`, `origem`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `utilizadores1`
+-- Estrutura da tabela `requisicao`
 --
 
-CREATE TABLE IF NOT EXISTS `utilizadores1` (
-`idUser` int(11) NOT NULL,
-  `Login` varchar(40) NOT NULL,
-  `Nome` varchar(40) NOT NULL,
-  `Senha` varchar(40) NOT NULL,
-  `Email` varchar(40) NOT NULL,
-  `Nivel` int(1) NOT NULL DEFAULT '1',
-  `Ativo` tinyint(1) NOT NULL DEFAULT '1',
-  `dtRegisto` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+CREATE TABLE `requisicao` (
+  `idReq` int(6) UNSIGNED NOT NULL,
+  `idUser` int(6) UNSIGNED NOT NULL,
+  `dataRequisicao` date NOT NULL,
+  `estado` bit(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Extraindo dados da tabela `utilizadores1`
+-- Extraindo dados da tabela `requisicao`
 --
 
-INSERT INTO `utilizadores1` (`idUser`, `Login`, `Nome`, `Senha`, `Email`, `Nivel`, `Ativo`, `dtRegisto`) VALUES
-(1, 'admin', 'Administrador', 'admin', 'admin@nowhere.com', 3, 1, '0000-00-00 00:00:00'),
-(2, 'f828', '', 'pmpreis@gmail.com', '123', 1, 1, '0000-00-00 00:00:00'),
-(3, 'f828', 'Paulo Reis', '123', 'pmpreis@gmail.com', 1, 1, '0000-00-00 00:00:00');
+INSERT INTO `requisicao` (`idReq`, `idUser`, `dataRequisicao`, `estado`) VALUES
+(1, 3, '2018-05-10', b'1');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `temprequisicao`
+--
+
+CREATE TABLE `temprequisicao` (
+  `sessao` varchar(50) NOT NULL,
+  `idLivro` int(6) NOT NULL,
+  `quantidade` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `temprequisicao`
+--
+
+INSERT INTO `temprequisicao` (`sessao`, `idLivro`, `quantidade`) VALUES
+('7ckpuqltdpophc3r8p70ru6buu', 1, 1),
+('7ckpuqltdpophc3r8p70ru6buu', 8, 1),
+('7ckpuqltdpophc3r8p70ru6buu', 9, 0),
+('bvkdsu75qko9jp3nuc26qde4lo', 9, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `utilizadores`
+--
+
+CREATE TABLE `utilizadores` (
+  `idUser` int(6) NOT NULL,
+  `login` varchar(40) NOT NULL,
+  `nome` varchar(40) NOT NULL,
+  `senha` varchar(40) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `nivel` bit(1) NOT NULL DEFAULT b'1',
+  `ativo` bit(1) NOT NULL DEFAULT b'1',
+  `dtRegisto` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `utilizadores`
+--
+
+INSERT INTO `utilizadores` (`idUser`, `login`, `nome`, `senha`, `email`, `nivel`, `ativo`, `dtRegisto`) VALUES
+(1, 'admin', 'Administrador', 'admin', 'admin@nowhere.com', b'1', b'1', '0000-00-00 00:00:00'),
+(3, 'f828', 'Paulo Reis', '123', 'pmpreis@gmail.com', b'1', b'1', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -209,43 +261,54 @@ INSERT INTO `utilizadores1` (`idUser`, `Login`, `Nome`, `Senha`, `Email`, `Nivel
 -- Indexes for table `categorias`
 --
 ALTER TABLE `categorias`
- ADD PRIMARY KEY (`idCat`);
-
---
--- Indexes for table `categorias1`
---
-ALTER TABLE `categorias1`
- ADD PRIMARY KEY (`idCat`);
+  ADD PRIMARY KEY (`idCat`);
 
 --
 -- Indexes for table `editoras`
 --
 ALTER TABLE `editoras`
- ADD PRIMARY KEY (`idEditora`);
+  ADD PRIMARY KEY (`idEditora`);
 
 --
 -- Indexes for table `idiomas`
 --
 ALTER TABLE `idiomas`
- ADD PRIMARY KEY (`idIdioma`);
+  ADD PRIMARY KEY (`idIdioma`);
 
 --
--- Indexes for table `livros1`
+-- Indexes for table `livros`
 --
-ALTER TABLE `livros1`
- ADD PRIMARY KEY (`idLivro`);
+ALTER TABLE `livros`
+  ADD PRIMARY KEY (`idLivro`),
+  ADD KEY `idCat` (`idCat`),
+  ADD KEY `idEditora` (`idEditora`),
+  ADD KEY `idIdioma` (`idIdioma`),
+  ADD KEY `idOrigem` (`idOrigem`);
 
 --
 -- Indexes for table `origem`
 --
 ALTER TABLE `origem`
- ADD PRIMARY KEY (`idOrigem`);
+  ADD PRIMARY KEY (`idOrigem`);
 
 --
--- Indexes for table `utilizadores1`
+-- Indexes for table `requisicao`
 --
-ALTER TABLE `utilizadores1`
- ADD PRIMARY KEY (`idUser`);
+ALTER TABLE `requisicao`
+  ADD PRIMARY KEY (`idReq`);
+
+--
+-- Indexes for table `temprequisicao`
+--
+ALTER TABLE `temprequisicao`
+  ADD PRIMARY KEY (`sessao`,`idLivro`),
+  ADD KEY `idLivro` (`idLivro`);
+
+--
+-- Indexes for table `utilizadores`
+--
+ALTER TABLE `utilizadores`
+  ADD PRIMARY KEY (`idUser`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -255,37 +318,58 @@ ALTER TABLE `utilizadores1`
 -- AUTO_INCREMENT for table `categorias`
 --
 ALTER TABLE `categorias`
-MODIFY `idCat` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
---
--- AUTO_INCREMENT for table `categorias1`
---
-ALTER TABLE `categorias1`
-MODIFY `idCat` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCat` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
 --
 -- AUTO_INCREMENT for table `editoras`
 --
 ALTER TABLE `editoras`
-MODIFY `idEditora` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+  MODIFY `idEditora` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- AUTO_INCREMENT for table `idiomas`
 --
 ALTER TABLE `idiomas`
-MODIFY `idIdioma` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `idIdioma` tinyint(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
--- AUTO_INCREMENT for table `livros1`
+-- AUTO_INCREMENT for table `livros`
 --
-ALTER TABLE `livros1`
-MODIFY `idLivro` int(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+ALTER TABLE `livros`
+  MODIFY `idLivro` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
 -- AUTO_INCREMENT for table `origem`
 --
 ALTER TABLE `origem`
-MODIFY `idOrigem` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `idOrigem` tinyint(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
--- AUTO_INCREMENT for table `utilizadores1`
+-- AUTO_INCREMENT for table `requisicao`
 --
-ALTER TABLE `utilizadores1`
-MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+ALTER TABLE `requisicao`
+  MODIFY `idReq` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `utilizadores`
+--
+ALTER TABLE `utilizadores`
+  MODIFY `idUser` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `livros`
+--
+ALTER TABLE `livros`
+  ADD CONSTRAINT `livros_ibfk_1` FOREIGN KEY (`idCat`) REFERENCES `categorias` (`idCat`),
+  ADD CONSTRAINT `livros_ibfk_2` FOREIGN KEY (`idEditora`) REFERENCES `editoras` (`idEditora`),
+  ADD CONSTRAINT `livros_ibfk_3` FOREIGN KEY (`idIdioma`) REFERENCES `idiomas` (`idIdioma`),
+  ADD CONSTRAINT `livros_ibfk_4` FOREIGN KEY (`idOrigem`) REFERENCES `origem` (`idOrigem`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
